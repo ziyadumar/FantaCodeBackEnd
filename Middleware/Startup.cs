6f86a0768e0cfaf7e3478aaa90081279011f3c70
ZIYAD UMAR <ziyad96@gmail.com>
2017-12-15 15:16:46 +0530

    changes to middleware

--- Middleware/Startup.cs
+++ Middleware/Startup.cs
@@ -13,10 +13,31 @@ public class Startup
         });
     }
 
+    
+    private static void HandleMapTest1(IApplicationBuilder app)
+    {
+        app.Run(async context =>
+        {
+            await context.Response.WriteAsync("Map Test 1");
+        });
+    }
+
+    private static void HandleMapTest2(IApplicationBuilder app)
+    {
+        app.Run(async context =>
+        {
+            await context.Response.WriteAsync("Map Test 2");
+        });
+    }
+
     public void Configure(IApplicationBuilder app)
     {
         app.MapWhen(context => context.Request.Query.ContainsKey("branch"),
                                HandleBranch);
+
+        app.Map("/map1", HandleMapTest1);
+
+        app.Map("/map2", HandleMapTest2);
 
         app.Run(async context =>
         {
